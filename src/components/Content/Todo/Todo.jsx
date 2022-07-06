c0d4efc05a6fef666d0e7f5395e3b58c611346ad
alex-sikach <hlophlop1245@gmail.com>
2022-07-06 15:07:52 +0300

    auto scroll to the lastest todo

--- src/components/Content/Todo/Todo.jsx
+++ src/components/Content/Todo/Todo.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import style from './Todo.module.css'
 import TodoItemContainer from "./TodoItem/TodoItemContainer";
+import {Profiler} from "react";
 
 const Todo = (props) => {
+
+    const todosEl = React.createRef();
+
+    function scrollDownLast() {
+        // todosEl.current.scrollTo({top: 0})
+        let latestTodo = todosEl.current.children[
+            todosEl.current.children.length-1
+        ];
+        if(latestTodo) {
+            latestTodo.scrollIntoView()
+        }
+    }
 
     const isEnter = (e) => e.code === 'Enter' ? addTask() : '';
 
@@ -33,13 +46,15 @@ const Todo = (props) => {
                     placeholder="Todo: "/>
                 <svg onClick={addTask} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill='#212121' d="M432 256c0 17.69-14.33 32.01-32 32.01H256v144c0 17.69-14.33 31.99-32 31.99s-32-14.3-32-31.99v-144H48c-17.67 0-32-14.32-32-32.01s14.33-31.99 32-31.99H192v-144c0-17.69 14.33-32.01 32-32.01s32 14.32 32 32.01v144h144C417.7 224 432 238.3 432 256z"/></svg>
             </div>
-            <div className={style.content}>
-                {
-                    props.state.todos.map(todo =>
+            <Profiler id="div" onRender={scrollDownLast}>
+                <div className={style.content} ref={todosEl}>
+                    {
+                        props.state.todos.map(todo =>
                             todo.task ? <TodoItemContainer id={todo.id} task={todo.task} /> : ''
-                    )
-                }
-            </div>
+                        )
+                    }
+                </div>
+            </Profiler>
         </div>
     )
 }
