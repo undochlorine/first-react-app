07fcc73b61cc25038023299a9a5e2087fd96bfaa
alex-sikach <hlophlop1245@gmail.com>
2022-07-19 18:56:47 +0300

    auto scroll to the latest msg + smooth

--- src/components/Content/Dialogs/Dialog/Chat/Chat.jsx
+++ src/components/Content/Dialogs/Dialog/Chat/Chat.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import style from './Chat.module.css'
+import {Profiler} from "react";
 
 const Chat = (props) => {
     const messages_jsx = props.chat_history.map(obj => <div className={style[obj.from]}><p>{obj.message}</p></div>);
     // document.querySelector('.' + style.Main).scrollTo({ bottom: 0 });
 
+    const messages = React.createRef()
+
+    function scrollDownLast() {
+        let latestMsg = messages.current.children[
+        messages.current.children.length-1
+            ];
+        if(latestMsg) {
+            latestMsg.scrollIntoView({behavior: 'smooth'})
+        }
+    }
+
     return(
-        <div className={style.Main}>
-            {messages_jsx}
-        </div>
+        <Profiler id="div" onRender={scrollDownLast}>
+            <div className={style.Main} ref={messages}>
+                {messages_jsx}
+            </div>
+        </Profiler>
     )
 }
 
