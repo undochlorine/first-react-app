640c02b30e77c7622a2dfc528ca607a493fcbadf
alex-sikach <hlophlop1245@gmail.com>
2022-08-02 20:24:50 +0300

    server folder structure: paths folder

--- src/server/index.js
+++ src/server/index.js
@@ -4,51 +4,18 @@ const pool = require('./pool')
 
 const app = express()
 
-const port = 5000;
-
 //middleware
 app.use(cors())
 app.use(express.json())
 
+const port = 5000;
+
 //ROUTES//
+require('./paths/usersList').map(options => app.get(...options))
 
-//get some users
-app.get('/users/:maxId/:limit', async (req, res) => {
-    try {
-        const { maxId, limit } = req.params
-        const users = (await pool.query(
-            'SELECT * FROM users WHERE id > $1 LIMIT $2',
-            [maxId, limit]
-        )).rows
-        res.json(users)
-    } catch(e) {
-        console.error(e.message)
-    }
-})
-
-/*app.post("", async (req, res) => {
-    try {
-
-    } catch (e) {
-        console.error(e.message);
-    }
-});*/
-
-/*app.put("/users/:id", async (req, res) => {
-    try {
-
-    } catch (e) {
-        console.error(e.message);
-    }
-});*/
-
-/*app.delete("/users/:id", async (req, res) => {
-    try {
-
-    } catch (e) {
-        console.log(e.message);
-    }
-});*/
+//middleware
+app.use(cors())
+app.use(express.json())
 
 app.listen(port, () => {
     console.log(`server has started on port ${port}`)
---
+++ src/server/paths/usersList.js
@@ -0,0 +1,43 @@
+const pool = require('../pool')
+
+const getUsers = [
+    '/users/:maxId/:limit',
+    async (req, res) => {
+        try {
+            const { maxId, limit } = req.params
+            const users = (await pool.query(
+                'SELECT * FROM users WHERE id > $1 LIMIT $2',
+                [maxId, limit]
+            )).rows
+            res.json(users)
+        } catch(e) {
+            console.error(e.message)
+        }
+    }
+]
+
+/*app.post("", async (req, res) => {
+    try {
+
+    } catch (e) {
+        console.error(e.message);
+    }
+});*/
+
+/*app.put("/users/:id", async (req, res) => {
+    try {
+
+    } catch (e) {
+        console.error(e.message);
+    }
+});*/
+
+/*app.delete("/users/:id", async (req, res) => {
+    try {
+
+    } catch (e) {
+        console.log(e.message);
+    }
+});*/
+
+module.exports = [getUsers]
