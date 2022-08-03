30153447f7b9b784db24a2d92ce62f256b4f120a
alex-sikach <hlophlop1245@gmail.com>
2022-08-03 14:32:39 +0300

    port = process.env.PORT || 5000

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //ROUTES//
 require('./paths/usersList').map(options => app.get(...options))
