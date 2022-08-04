df58f2ab91e77e59267d75ca5a2e76344a1c830c
alex-sikach <hlophlop1245@gmail.com>
2022-08-04 14:28:11 +0300

    UsersList - Class Component

--- src/components/Content/UsersList/UsersList.js
+++ src/components/Content/UsersList/UsersList.js
@@ -3,79 +3,90 @@ import style from './styles/UsersList.module.css'
 import showMoreStyle from './styles/showMore.module.css'
 import friendStyle from './styles/friend.module.css'
 
-const UsersList = (props) => {
-    function maxId() {
-        let max = props.users[0]?.id;
-        for (let i = 0; i < props.users.length; i++) {
-            if (props.users[i].id > max) {
-                max = props.users[i].id
+class UsersList extends React.Component {
+
+    constructor(props) {
+        super(props);
+        //initial users batch
+        if(props.users.length === 0) {
+            this.props.addUsers(0, 2)
+        }
+    }
+
+    maxId = () => {
+        let max = this.props.users[0]?.id;
+        for (let i = 0; i < this.props.users.length; i++) {
+            if (this.props.users[i].id > max) {
+                max = this.props.users[i].id
             }
         }
         return max;
     }
 
-    function addMore() {
-        props.addUsers(maxId(), 2)
+    addMore = () => {
+        this.props.addUsers(this.maxId(), 2)
     }
 
-    function switchFriendState({state, id}) {
+    switchFriendState = ({state, id}) => {
         switch (state) {
             case true:
-                props.unfriend(id);
+                this.props.unfriend(id);
                 break;
             case false:
-                props.friend(id);
+                this.props.friend(id);
                 break;
             default:
                 return 0;
         }
     }
 
-    return (
-        <div className={style.Main}>
-            <div className={style.wrapper}>
-                {props.users.map(user => {
-                    return (
-                        <div className={style.user} key={user.id}>
-                            <div className={style.avatar}>
-                                <img src={user.avatar} alt="User avatar"/>
-                            </div>
-                            <div className={style.info}>
-                                <div className={style.fullname}>
-                                    <p>{user.name} {user.lastname}</p>
+    render() {
+        return (
+            <div className={style.Main}>
+                <div className={style.wrapper}>
+                    {this.props.users.map(user => {
+                        return (
+                            <div className={style.user} key={user.id}>
+                                <div className={style.avatar}>
+                                    <img src={user.avatar} alt="User avatar"/>
                                 </div>
-                                <div className={style.status}>
-                                    <p>{user.status}</p>
+                                <div className={style.info}>
+                                    <div className={style.fullname}>
+                                        <p>{user.name} {user.lastname}</p>
+                                    </div>
+                                    <div className={style.status}>
+                                        <p>{user.status}</p>
+                                    </div>
+                                    <div className={style.location}>
+                                        <p>{user.location.country}, {user.location.city}</p>
+                                    </div>
                                 </div>
-                                <div className={style.location}>
-                                    <p>{user.location.country}, {user.location.city}</p>
+                                <div className={friendStyle.subscribe}>
+                                    <button
+                                        onClick={() => this.switchFriendState({
+                                            state: user.friend,
+                                            id: user.id
+                                        })}
+                                    >{user.friend ? 'Unfriend' : 'To friend'}</button>
                                 </div>
                             </div>
-                            <div className={friendStyle.subscribe}>
-                                <button
-                                    onClick={() => switchFriendState({
-                                        state: user.friend,
-                                        id: user.id
-                                    })}
-                                >{user.friend ? 'Unfriend' : 'To friend'}</button>
-                            </div>
-                        </div>
-                    )
-                })}
-                <div className={showMoreStyle}>
-                    <button
-                        className={showMoreStyle.showMoreBtn}
-                        onClick={addMore}
-                    >
-                        <span className={showMoreStyle.showMoreSpan}></span>
-                        <span className={showMoreStyle.showMoreSpan}></span>
-                        <span className={showMoreStyle.showMoreSpan}></span>
-                        <span className={showMoreStyle.showMoreSpan}></span>Show more
-                    </button>
+                        )
+                    })}
+                    <div className={showMoreStyle}>
+                        <button
+                            className={showMoreStyle.showMoreBtn}
+                            onClick={this.addMore}
+                        >
+                            <span className={showMoreStyle.showMoreSpan}></span>
+                            <span className={showMoreStyle.showMoreSpan}></span>
+                            <span className={showMoreStyle.showMoreSpan}></span>
+                            <span className={showMoreStyle.showMoreSpan}></span>Show more
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default UsersList
