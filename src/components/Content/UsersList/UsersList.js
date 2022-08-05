07d3a685e948143c211aefb59b823e077b122947
alex-sikach <hlophlop1245@gmail.com>
2022-08-05 13:54:57 +0300

    UsersList-func,UsersListContainer-class, export def connect()()

--- src/components/Content/UsersList/UsersList.js
+++ src/components/Content/UsersList/UsersList.js
@@ -3,90 +3,52 @@ import style from './styles/UsersList.module.css'
 import showMoreStyle from './styles/showMore.module.css'
 import friendStyle from './styles/friend.module.css'
 
-class UsersList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    componentDidMount() {
-        if(this.props.users.length === 0) {
-            this.props.addUsers(0, 2)
-        }
-    }
-
-    maxId = () => {
-        let max = this.props.users[0]?.id;
-        for (let i = 0; i < this.props.users.length; i++) {
-            if (this.props.users[i].id > max) {
-                max = this.props.users[i].id
-            }
-        }
-        return max;
-    }
-
-    addMore = () => {
-        this.props.addUsers(this.maxId(), 2)
-    }
-
-    switchFriendState = ({state, id}) => {
-        switch (state) {
-            case true:
-                this.props.unfriend(id);
-                break;
-            case false:
-                this.props.friend(id);
-                break;
-            default:
-                return 0;
-        }
-    }
-
-    render() {
-        return (
-            <div className={style.Main}>
-                <div className={style.wrapper}>
-                    {this.props.users.map(user => {
-                        return (
-                            <div className={style.user} key={user.id}>
-                                <div className={style.avatar}>
-                                    <img src={user.avatar} alt="User avatar"/>
+const UsersList = (props) => {
+    return (
+        <div className={style.Main}>
+            <div className={style.wrapper}>
+                {props.users.map(user => {
+                    return (
+                        <div className={style.user} key={user.id}>
+                            <div className={style.avatar}>
+                                <img src={user.avatar} alt="User avatar"/>
+                            </div>
+                            <div className={style.info}>
+                                <div className={style.fullname}>
+                                    <p>{user.name} {user.lastname}</p>
                                 </div>
-                                <div className={style.info}>
-                                    <div className={style.fullname}>
-                                        <p>{user.name} {user.lastname}</p>
-                                    </div>
-                                    <div className={style.status}>
-                                        <p>{user.status}</p>
-                                    </div>
-                                    <div className={style.location}>
-                                        <p>{user.location.country}, {user.location.city}</p>
-                                    </div>
+                                <div className={style.status}>
+                                    <p>{user.status}</p>
                                 </div>
-                                <div className={friendStyle.subscribe}>
-                                    <button
-                                        onClick={() => this.switchFriendState({
-                                            state: user.friend,
-                                            id: user.id
-                                        })}
-                                    >{user.friend ? 'Unfriend' : 'To friend'}</button>
+                                <div className={style.location}>
+                                    <p>{user.location.country}, {user.location.city}</p>
                                 </div>
                             </div>
-                        )
-                    })}
-                    <div className={showMoreStyle}>
-                        <button
-                            className={showMoreStyle.showMoreBtn}
-                            onClick={this.addMore}
-                        >
-                            <span className={showMoreStyle.showMoreSpan}></span>
-                            <span className={showMoreStyle.showMoreSpan}></span>
-                            <span className={showMoreStyle.showMoreSpan}></span>
-                            <span className={showMoreStyle.showMoreSpan}></span>Show more
-                        </button>
-                    </div>
+                            <div className={friendStyle.subscribe}>
+                                <button
+                                    onClick={() => props.switchFriendState({
+                                        state: user.friend,
+                                        id: user.id
+                                    })}
+                                >{user.friend ? 'Unfriend' : 'To friend'}</button>
+                            </div>
+                        </div>
+                    )
+                })}
+                <div className={showMoreStyle}>
+                    <button
+                        className={showMoreStyle.showMoreBtn}
+                        onClick={props.addMore}
+                    >
+                        <span className={showMoreStyle.showMoreSpan}></span>
+                        <span className={showMoreStyle.showMoreSpan}></span>
+                        <span className={showMoreStyle.showMoreSpan}></span>
+                        <span className={showMoreStyle.showMoreSpan}></span>Show more
+                    </button>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    )
 
+}
 export default UsersList
--- src/components/Content/UsersList/UsersListContainer.js
+++ src/components/Content/UsersList/UsersListContainer.js
@@ -3,6 +3,55 @@ import {friendAC, unfriendAC, addUsersAC} from "../../../redux/reducers/users-li
 import {connect} from "react-redux";
 import UsersList from "./UsersList";
 
+class UsersListContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            this.props.addUsers(0, 2)
+        }
+    }
+
+    maxId = () => {
+        let max = this.props.users[0]?.id;
+        for (let i = 0; i < this.props.users.length; i++) {
+            if (this.props.users[i].id > max) {
+                max = this.props.users[i].id
+            }
+        }
+        return max;
+    }
+
+    addMore = () => {
+        this.props.addUsers(this.maxId(), 2)
+    }
+
+    switchFriendState = ({state, id}) => {
+        switch (state) {
+            case true:
+                this.props.unfriend(id);
+                break;
+            case false:
+                this.props.friend(id);
+                break;
+            default:
+                return 0;
+        }
+    }
+
+    render() {
+        return(
+            <UsersList
+                users={this.props.users}
+                switchFriendState={this.switchFriendState}
+                addMore={this.addMore}
+            />
+        )
+    }
+}
+
 function mapStateToProps(state) {
     return {
         users: state.usersList.users
@@ -25,5 +74,4 @@ function mapDispatchToProps(dispatch) {
     }
 }
 
-const UsersListContainer = connect(mapStateToProps, mapDispatchToProps)(UsersList)
-export default UsersListContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersListContainer)
